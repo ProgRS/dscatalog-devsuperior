80236d5e1f710c64f3c0bdbbe94cf437d4585e5b
ProgRs <lfernandotc@hotmail.com>
2023-02-01 21:36:32 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,12 +1,25 @@
 package com.devsuperior.dscatalog.entities;
 
 import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import java.io.Serializable;
 
+
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-		private Long id;
+		
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
 		private String name;
 		
 		public Category() {
