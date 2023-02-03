d3a4f907e0d91a16cc107bf12234f5693592c2b8
ProgRs <lfernandotc@hotmail.com>
2023-02-02 21:31:48 -0300

    Buscando categoria por ID

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,7 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +34,15 @@ public class CategoryService {
 	    
 	    //return listDto;
 	    
+	}
+  
+	@Transactional(readOnly = true) 
+	public CategoryDTO finById(Long id) {
+		
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
+		
 	}
 	
 	
